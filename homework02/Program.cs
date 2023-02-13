31604e1a9682466d1a12e49d2090aa9cbf66c8f9
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-13 21:10:04 +0300

    Add homework #2

--- homework02/Program.cs
+++ homework02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int maxNumber = numberA;
+if (numberB > maxNumber) 
+{
+    maxNumber = numberB;
+}
+if (numberC > maxNumber)
+{
+    maxNumber = numberC;
+}
+Console.WriteLine($"{numberA}, {numberB}, {numberC} --> max: {maxNumber}");
