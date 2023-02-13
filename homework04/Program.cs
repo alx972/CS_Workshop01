77e1deb2418902543389a059f0c053a2db6b19a6
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-13 23:09:06 +0300

    Add homework #4

--- homework04/Program.cs
+++ homework04/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool bypass = false; // предустановленная переменная для проверки на ввод положительного числа
+int num = 0; // инициализация числа
+int count = 1; // инициализация счетчика цикла положительных чисел
+while (!bypass) // Проверка на ввод положительного числа
+{
+    Console.Write("Введите положительное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    if (num <= 0) 
+    {
+        Console.WriteLine("Ошибка ввода.");
+    }
+    else
+    {
+        bypass = true;
+    }
+}
+Console.Write($"{num} --> ");
+while (count <= num)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
