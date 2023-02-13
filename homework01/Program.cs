3bd47bcd2b55b2e6f687c8a42ec50d094821a550
Alex Slyussarev <alex.slyussarev@yandex.ru>
2023-02-13 20:48:16 +0300

    Add homework #1

---
+++ homework01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB) 
+{
+    Console.WriteLine($"Число A = {numberA} больше числа B = {numberB}");
+}
+else if (numberA < numberB)
+{
+    Console.WriteLine($"Число A = {numberA} меньше числа B = {numberB}");
+}
+else
+{
+    Console.WriteLine($"Число A = {numberA} равно числу B = {numberB}");
+}
